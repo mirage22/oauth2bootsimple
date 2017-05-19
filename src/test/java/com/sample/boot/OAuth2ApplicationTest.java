b83cf9448633e8c2619df9673a7b1d07f02f1c03
Miroslav Wengner <miro.wengner@gmail.com>
2017-05-19 20:27:35 +0200

    application context test

---
+++ src/test/java/com/sample/boot/OAuth2ApplicationTest.java
@@ -0,0 +1,19 @@
+package com.sample.boot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * @author Miro Wengner (@miragemiko)
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
+public class OAuth2ApplicationTest {
+
+    @Test
+    public void contextLoads() {
+    }
+    
+}
